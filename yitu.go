5435d8fc207957ee6e5dee128374093a98fc8fb2
halulu <i@halu.lu>
2019-02-26 17:26:46 +0800

    dir

--- yitu.go
+++ yitu.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 	"regexp"
 	"strings"
 
@@ -27,7 +28,7 @@ func main() {
 		Email:       "lzjluzijie@gmail.com",
 		Flags: []cli.Flag{
 			cli.StringFlag{
-				Name:  "path",
+				Name:  "p",
 				Usage: "filepath",
 			},
 		},
@@ -37,25 +38,44 @@ func main() {
 				panic("path is empty!")
 			}
 
-			log.Printf("Start to read %s", p)
-
-			f, err := ioutil.ReadFile(p)
+			fi, err := os.Stat(p)
 			if err != nil {
-				log.Println(err.Error())
-				return
-			}
-			old := string(f)
-
-			replaced, err := Replace(old)
-			if err != nil {
-				log.Println(err.Error())
-				return
+				panic(err.Error())
 			}
 
-			err = ioutil.WriteFile(p, []byte(replaced), 0600)
-			if err != nil {
-				log.Println(err.Error())
-				return
+			if !fi.IsDir() {
+				log.Printf("%s is a file", p)
+				err = HandleFile(p)
+				if err != nil {
+					log.Println(err.Error())
+				}
+			} else {
+				log.Printf("%s is a dir", p)
+				list := make([]string, 0)
+				err = filepath.Walk(p, func(p string, fi os.FileInfo, err error) error {
+					if err != nil {
+						panic(err)
+					}
+
+					if IsMarkdown(p) {
+						list = append(list, p)
+						return nil
+					}
+
+					log.Printf("%s is not a markdown file, skipped", p)
+					return nil
+				})
+				if err != nil {
+					panic(err)
+				}
+
+				log.Printf("markdown files: %v", list)
+				for _, p := range list {
+					err = HandleFile(p)
+					if err != nil {
+						log.Println(err.Error())
+					}
+				}
 			}
 
 			log.Println("done")
@@ -65,8 +85,43 @@ func main() {
 
 	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Println(err.Error())
 	}
+	return
+}
+
+func IsMarkdown(p string) (is bool) {
+	is = false
+	if path.Ext(p) == ".md" ||
+		path.Ext(p) == ".markdown" ||
+		path.Ext(p) == ".mdown" ||
+		path.Ext(p) == ".mkdn" ||
+		path.Ext(p) == ".mkd" ||
+		path.Ext(p) == ".mdwn" ||
+		path.Ext(p) == ".mdtxt" ||
+		path.Ext(p) == ".mdtext" {
+		is = true
+	}
+	return
+}
+
+func HandleFile(p string) (err error) {
+	f, err := ioutil.ReadFile(p)
+	if err != nil {
+		return
+	}
+	old := string(f)
+
+	replaced, err := Replace(old)
+	if err != nil {
+		return
+	}
+
+	err = ioutil.WriteFile(p, []byte(replaced), 0600)
+	if err != nil {
+		return
+	}
+	return
 }
 
 type Image struct {
